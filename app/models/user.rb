24b83be5a44a69cd03dc85de673ba3332476eeb3
RUSTAMZA <masumazeri@hotmail.com>
2021-12-01 16:20:19 +0000

    W4D2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
 
-    has_many :finstagram_posts
-    has_many :comments
-    has_many :likes
-  
-  end
+  has_many :finstagram_posts
+  has_many :comments
+  has_many :likes
+
+
+  validates :email, :username, uniqueness: true
+  validates :email, :avatar_url, :username, :password, presence: true
+
+
+end
